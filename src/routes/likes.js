746e3e9408bf70650347e68dae594fe003bc05eb
kshiprashetty <kshiprashetty@gmail.com>
2018-02-12 06:02:56 +0530

    added likes

---
+++ src/routes/likes.js
@@ -0,0 +1,47 @@
+const models = require('../../models');
+
+module.exports = [
+  {
+    path: '/books/{bookID}/like',
+    method: 'POST',
+    handler: (request, response) => {
+      const id = request.params.bookID;
+
+      models.books.findOne({
+        where: {
+          bookID: id,
+        },
+      })
+        .then((book) => {
+          if (book === null) {
+            throw new Error(`Could not find book with id: ${id}.`);
+          }
+
+          return book.updateAttributes({
+            like: true,
+          });
+        })
+        .then(() => {
+          response({
+            data: {
+              reason: 'Liked!',
+              statusCode: 204,
+            },
+          });
+        }, (reason) => {
+          response({
+            data: { reason: reason.message },
+            statusCode: 404,
+          });
+        })
+        .catch(() => {
+          response({
+            data: {
+              reason: 'Could not update book attributes.',
+            },
+            statusCode: 500,
+          });
+        });
+    },
+  },
+];
